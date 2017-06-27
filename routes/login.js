f7f8cc4cddc5b4fda11ec080fe314df82ecb31d6
Daseul Bae <ektmf1993@gmail.com>
2017-06-27 16:15:09 +0900

    login exception

--- routes/login.js
+++ routes/login.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/db_pool.js');
 const jwt = require('jsonwebtoken');
 const async = require('async');
+const pool = require('../config/db_pool.js');
 
 router.post('/', async function(req, res) {
-    console.log(req.body);
     try {
-        var connection = await pool.getConnection();
         const email = req.body.email;
         const password = req.body.password;
+        var connection = await pool.getConnection();
         let query = 'select * from Signup where email = ?';
         let data = await connection.query(query, email) || null;
-        if(password != data[0].password) res.status(401).send({message: 'wrong email or password'});
+        //아이디가 존재하지 않을 경우
+        if(data.length == 0) {
+            res.status(401).send({message: 'wrong email'});
+        }
+        //비밀번호가 틀릴 경우
+        else if(password != data[0].password) {
+            res.status(401).send({message: 'wrong password'});
+        }
         else {
-            console.log(data[0].ID);
             //jwt 발급하고 성공메세지 보내주기
             const secretKey = req.app.get('jwt-secret');
-            console.log(secretKey);
             const options = {
                 algorithm : "HS256",
                 expiresIn : 60 * 60 * 24 * 30//30 days
@@ -26,7 +30,6 @@ router.post('/', async function(req, res) {
                 user_id : data[0].ID
             };
             const token = jwt.sign(payload, secretKey, options);
-            console.log(token);
             res.status(200).send({message : 'login success', token : token});
         }
     }
